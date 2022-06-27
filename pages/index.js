3af896b1402910b4c9342b130b4f0842faa02579
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-27 11:49:10 +0200

    Changed styles import and added all components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import About from '../components/About'
+import HowToCustom from '../components/HowToCustom'
+import Categories from '../components/Categories'
+import Testimony from '../components/Testimony'
+import Footer from '../components/Footer'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -10,11 +17,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+      <Navbar />
+      <Header />
+      <About />
+      <HowToCustom />
+      <Categories />
+      <Testimony />
+      <Footer />
     </div>
   )
 }
