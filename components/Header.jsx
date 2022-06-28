f55fa0f347bfd8159fcc2c991f20a57856b8cd07
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-28 10:54:26 +0200

    header jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
 import styles from '../styles/Header.module.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className={styles.headerContainer}>
+      <div className={styles.headerLeft}>
+        <h3>
+          Are you looking for <span>woodden furniture</span> for your place?
+        </h3>
+        <h1>This is the Right Place</h1>
+        <a href="#categories" className={styles.headerBtn}>
+          Explore Furniture
+        </a>
+      </div>
+
+      <div className={styles.headerRight}>
+        <Image
+          src="/assets/headerTable.png"
+          alt="table"
+          width={568}
+          height={577}
+        />
+      </div>
+    </header>
   )
 }
 
