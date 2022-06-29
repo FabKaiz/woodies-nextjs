c14b414812183c7d505c701a1a7ab41bf3f6be0d
Fabien Chareun <cacrfabfr@gmail.com>
2022-06-29 14:44:04 +0200

    started footer jsx and scss

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,20 @@ import styles from '../styles/Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      {/* div newsletters */}
+      <div className={styles.newsletterContainer}>
+        <div>
+          <h2>Subscribe to our newsletter</h2>
+          <p>A monthly digest of the new WOODIES products, hot offers, and resources.</p>
+        </div>
+        <div className={styles.inputContainer}>
+          <input type="text" placeholder="Your email address" />
+          <button className={styles.inputBtn}>Subscribe</button>
+        </div>
+      </div>
+      {/* div bottom */}
+    </footer>
   )
 }
 
